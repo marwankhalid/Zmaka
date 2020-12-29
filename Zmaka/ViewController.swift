d374bc8448043bf38286a478b5df429dc6192c29
Nomen Nescio <33214208+marwankhalid@users.noreply.github.com>
2020-12-29 11:08:20 +0500

    Initial Commit

---
+++ Zmaka/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Zmaka
+//
+//  Created by Marwan Khalid on 12/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
