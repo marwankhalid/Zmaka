03d579229d86df43a4ce0a6e67b6d6866c36eb3b
Marwan Khalid <33214208+marwankhalid@users.noreply.github.com>
2020-12-29 16:31:46 +0500

    connect to Firebase and design the Login

--- Zmaka/ViewController.swift
+++ Zmaka/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import TextFieldFloatingPlaceholder
 
 class ViewController: UIViewController {
 
+    @IBOutlet var password: TextFieldFloatingPlaceholder!
+    @IBOutlet var emailAddress: TextFieldFloatingPlaceholder!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        password.validationTrueLineEditingColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        emailAddress.validationTrueLineEditingColor = #colorLiteral(red: 0, green: 0.4784313725, blue: 1, alpha: 1)
+        password.validationTrueLineColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        emailAddress.validationTrueLineColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+            
     }
-
-
 }
 
